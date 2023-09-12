b556791a3ad9aea18fce8bbddbc69bd354cb3d1a
이소라 <131628924+leesr94@users.noreply.github.com>
2023-09-12 20:30:23 +0900

    Create 20230912 최대공약수와 최소공배수.java

---
+++ Programmers_Lv.1/20230912 최대공약수와 최소공배수.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int max = GDC(n, m);
+        int min = n * m / max;
+        
+        int[] answer = { max, min };
+        return answer;
+    }
+    
+    public static int GDC(int a, int b) {
+        if (a < b) {		// a가 b보다 작으면 값 변경
+            int tmp = a;
+            a = b;
+            b = tmp;
+        }
+        
+        while (b != 0) {	// b가 0이 될 때까지 나누기
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        
+        return a;
+    }
+}
