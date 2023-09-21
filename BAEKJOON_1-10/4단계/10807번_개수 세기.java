96d32d3cdccac142e7043787c7f71d3e8cbc7c88
이소라 <131628924+leesr94@users.noreply.github.com>
2023-09-21 16:03:46 +0900

    Create 10807번_개수 세기.java

---
+++ BAEKJOON_1-10/4단계/10807번_개수 세기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int v = sc.nextInt();
+		int cnt = 0;
+		
+		for (int i : arr) {
+			if (v == i) cnt++;
+		}
+		
+		System.out.println(cnt);
+		
+		sc.close();
+		
+	}
+}
