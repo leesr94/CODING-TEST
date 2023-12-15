21c4042f11dde428f8666b671cee327058244863
이소라 <131628924+leesr94@users.noreply.github.com>
2023-12-15 18:49:57 +0900

    Create 9063번_대지.java

---
+++ BAEKJOON_1-10/10단계/9063번_대지.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int minX = 10000, minY = 10000, maxX = -10000, maxY = -10000;
+		
+		for (int i = 0; i < n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			minX = Math.min(minX, x);
+			minY = Math.min(minY, y);
+			maxX = Math.max(maxX, x);
+			maxY = Math.max(maxY, y);
+		}
+		
+		br.close();
+		
+		System.out.println((maxX - minX) * (maxY - minY));
+		
+	}
+}
