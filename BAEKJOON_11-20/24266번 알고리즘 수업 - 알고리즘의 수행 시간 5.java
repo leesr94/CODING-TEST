828ca9b4d03ad0a6e10f6c173e8b616e659b11b6
이소라 <131628924+leesr94@users.noreply.github.com>
2023-12-20 21:15:39 +0900

    Create 24266번 알고리즘 수업 - 알고리즘의 수행 시간 5.java

---
+++ BAEKJOON_11-20/24266번 알고리즘 수업 - 알고리즘의 수행 시간 5.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Integer.parseInt(br.readLine());
+		
+		System.out.println(n * n * n);
+		System.out.println(3);
+		
+	}
+}
