48d05bded285ca225555f2f6b6d0ab1dcc20913f
이소라 <131628924+leesr94@users.noreply.github.com>
2024-01-02 19:41:11 +0900

    Create k진수에서 소수 개수 구하기.java

---
+++ Programmers_Lv.2/k진수에서 소수 개수 구하기.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int k) {
+        List<Long> list = new ArrayList<>();
+        int answer = 0;
+        
+        for (String t : Integer.toString(n, k).split("0")) {
+            if (t.length() == 0 || t.equals("1")) continue;
+            list.add(Long.parseLong(t));
+        }
+        
+        for (long l : list) {
+            int tmp = 0;
+            
+            for (int i = 2; i <= Math.sqrt(l); i++) {
+                if (l % i == 0) tmp++;
+            }
+            
+            if (l == 2 || tmp == 0) answer++;
+        }
+        
+        return answer;
+    }
+}
