b4108e24f0a227bebe2da12162f65c61e9bab9c4
이소라 <131628924+leesr94@users.noreply.github.com>
2024-01-07 23:13:11 +0900

    Create 2587번 대표값2.java

---
+++ BAEKJOON_11-20/13단계/2587번 대표값2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int[] arr = new int[5];
+		int sum = 0;
+		
+		for (int i = 0; i < 5; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+		
+		Arrays.sort(arr);
+		
+		sb.append(sum / 5);
+		sb.append("\n");
+		sb.append(arr[2]);
+		
+		System.out.println(sb.toString());
+		
+	}
+}
